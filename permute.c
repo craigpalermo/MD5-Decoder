767f7dbafc420841f562a56648162ab7785a50d4
Craig Palermo <cpalermo2@gmail.com>
2013-04-09 14:24:46 -0400

    Commented out lines that print # of current permutations to improve efficiency.

--- permute.c
+++ permute.c
@@ -9,6 +9,10 @@
 int permute(int length, const char arg[]);
 void hash_test(const char arg[]);
 
+/* ------ STATIC VARIABLES ------------*/
+static time_t start, stop;   /* <--  set up variables to record computation time */
+static clock_t ticks;
+
 /* 
    The main function prompts the user to enter a 32 character long MD5 hash,
    then generates permutations of a set of characters starting with a
@@ -23,13 +27,26 @@ int main(void) {
   printf("Enter the MD5 hash to decrypt: ");
   scanf("%s", input_hash);
 
-  if (strlen(input_hash) == 32)
+  if (strlen(input_hash) == 32) {
     /* processing will stop after length of 10 is reached, since
        computation time will be incredibly long and impractical */
-    while (length <= 10 && permute(length, input_hash) == 0)
+    putchar('\n');
+
+    time(&start);
+  
+    while (length <= 10 && permute(length, input_hash) == 0) {
+      printf("Not found in length %d\n", length);
       length++;
-  else
+    }
+
+    time(&stop);
+  } else
     printf("Invalid hash entered, program terminated.\n");
+
+  /* print the computation time */
+  printf("\n\n");
+  printf("Used %0.2f seconds of CPU time. \n", (double)ticks/CLOCKS_PER_SEC);
+  printf("Computation time: %.0f seconds\n\n", difftime(stop, start));
 
   /* hash_test("CRAIG"); */
 
@@ -46,8 +63,6 @@ int permute(int length, const char arg[]) {
   int i, n, count = 1, index = 0, result = 0;
   int *a;
   char *temp;
-  time_t start, stop;   /* <--  set up variables to record computation time */
-  clock_t ticks;
   md5_state_t state;    /* <-- declare variables for MD5 function */
   md5_byte_t digest[16];
   char hex_output[33];
@@ -55,7 +70,6 @@ int permute(int length, const char arg[]) {
 
   const char set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; /* <-- this string contains all characters used in permutations */
   
-
   temp = malloc(sizeof(char) * (length + 1)); /* allocate space for temp */
   a = malloc(length * sizeof *a);
   n = sizeof set - 1;
@@ -68,12 +82,10 @@ int permute(int length, const char arg[]) {
   for (i = 0; i < length; ++i)
     a[i] = 0;
   
-  time(&start); /* <-- start computation timer */
-
   do {
     /* print a counter that displays how many perutations have been made 
        for strings of this length */
-    printf("%i -- ", count);
+    /*    printf("%i -- ", count); */
     count++;
 
     for (i = length - 1; i >= 0; --i) {
@@ -92,17 +104,18 @@ int permute(int length, const char arg[]) {
     for (di = 0; di < 16; di++)
       sprintf(hex_output + di * 2, "%02x", digest[di]);
 
-    printf("%s", temp); /* print the current temp as plain text string */
+    /* printf("%s", temp);  print the current temp as plain text string */
       
     /* compare generated hash to parameter hash, stop looping if match found */
     if (strncmp(hex_output, arg, 32) == 0) {
-      printf("\n\nFound: %s", temp);
+      printf("\nFound: %s", temp);
       result = 1;
       break;
     }
 
     index = 0; /* reset index for placing new chars in temp array */
-    putchar('\n'); /* print new line character */
+  
+  /* putchar('\n');  print new line character */
     
     for (i = 0; i < length; ++i)
       if (++a[i] < n) 
@@ -112,13 +125,6 @@ int permute(int length, const char arg[]) {
     
     ticks = clock();
   } while (i < length);
-
-  time(&stop);
-  
-  /* print the computation time */
-  printf("\n\n");
-  printf("Used %0.2f seconds of CPU time. \n", (double)ticks/CLOCKS_PER_SEC);
-  printf("Computation time: %.0f seconds\n\n", difftime(stop, start));
 
   free(a);
   free(temp);
